c61953a47995241f1d7b306bb96b764f2ac8b882
MahroshZahid <157431935+MahroshZahid@users.noreply.github.com>
2024-02-12 16:21:21 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 var message; // name of variable
 message = "Hello!" // storing the string hello in variable
 alert(message);
+
+function showMessage() {
+    alert('Hello everyone');
+}
+
+showMessage()
