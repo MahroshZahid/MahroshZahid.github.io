7ddb52b828a55581d81b2de71a8e81c90bdff172
MahroshZahid <157431935+MahroshZahid@users.noreply.github.com>
2024-02-24 00:41:21 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
-var message; // name of variable
-message = "Hello!" // storing the string hello in variable
-alert(message);
-
-function showMessage() {
-    alert('Hello everyone');
+ // creating function for greeting with input from user after button trigger
+function myFunction() {
+    var name = prompt("Enter Name");
+    var greeting = "Hi " + name + "! Thank you for visiting my website :)";
+    document.getElementById("greeting").innerHTML = greeting;
 }
 
-showMessage()
-function changeText(word, iteration){
-    for (var i=0; i<iteration;i++){
-    alert("Hello" + word + "!");
+function ChangeImageSize(){
+    var img = document.getElementById("image");
+    img.style.height = "500px";
+    img.style.width = "400px";
+}
+
+// Function to toggle dropdown visibility
+function toggleDropdown(dropdownId) {
+    var dropdown = document.getElementById(dropdownId);
+    if (dropdown.style.display === 'block') {
+        dropdown.style.display = 'none';
+    } else {
+        dropdown.style.display = 'block';
     }
 }
 
-changeText("Canada", 3)
+// Function to show dropdown
+function showDropdown(dropdownId) {
+    var dropdown = document.getElementById(dropdownId);
+    dropdown.style.display = 'block';
+}
 
-var maindiv = document.getElementById("main");
+// Function to hide dropdown
+function hideDropdown(dropdownId) {
+    var dropdown = document.getElementById(dropdownId);
+    dropdown.style.display = 'none';
+}
